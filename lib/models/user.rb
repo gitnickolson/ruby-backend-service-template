449c7c148ffe64b312a216e8f58eebb8f6398e7b
gitnickolson <nickolsonick@gmail.com>
2026-03-05 23:43:41 +0100

    Update mail address column to not be encrypted

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -7,12 +7,12 @@ module Models
     model_schema do
       primary_key :id
       String :display_name, null: false
-      String :encrypted_email, null: false
+      String :mail_address, null: false
       String :encrypted_password, null: false
       DateTime :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
 
-      index :encrypted_email, name: :unique_emails, unique: true
-      index :encrypted_email
+      index :mail_address, name: :unique_mail_addresses, unique: true
+      index :mail_address
     end
   end
 end
