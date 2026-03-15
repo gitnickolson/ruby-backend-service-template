cfd136457a58030c5a14545880e4e9f1304ef448
gitnickolson <nickolsonick@gmail.com>
2026-03-15 21:58:29 +0100

    Add `PasswordEncrypter`

---
+++ spec/utility/password_encrypter_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'bcrypt'
+
+RSpec.describe Utility::PasswordEncrypter do
+  describe '.call' do
+    let(:password) { 'test' }
+
+    it 'hashes the password and adds the pepper' do
+      hashed_password = described_class.call(password:)
+      bcrypt_hash = BCrypt::Password.new(hashed_password)
+
+      expect(bcrypt_hash == "#{password}#{EnvironmentFetcher.pepper}").to be true
+    end
+  end
+end
