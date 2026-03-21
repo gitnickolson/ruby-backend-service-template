6916a7e23b55d1fef8df109d2e5d96668987d3ff
gitnickolson <nickolsonick@gmail.com>
2026-03-21 14:05:56 +0100

    Add `Models::User` factory

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  to_create(&:save)
+
+  factory(:user, class: 'Models::User') do
+    username { 'foo' }
+    mail_address { 'foo@bar.com' }
+    password_hash { 'hashed_password' }
+    created_at { Time.new(2023, 5, 20, 12, 2, 2) }
+  end
+end
