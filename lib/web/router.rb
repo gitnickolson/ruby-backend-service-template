5c3c887b03b8b6c80eef7736952de42860b2a994
gitnickolson <nickolsonick@gmail.com>
2026-06-07 19:01:53 +0200

    Allow CORS for localhost

--- lib/web/router.rb
+++ lib/web/router.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
+require 'rack/cors'
 require 'sinatra/base'
 
 module Web
   class Router
     def initialize
-      @app =
-        Rack::Builder.app do
-          use OpenapiFirst::Middlewares::RequestValidation
-          use OpenapiFirst::Middlewares::ResponseValidation
-
-          map('/users') { run Web::Controllers::UsersController }
-        end
+      @app = initialize_app
     end
 
     def call(env)
@@ -21,5 +16,23 @@ module Web
     private
 
     attr_reader :app
+
+    def initialize_app # rubocop:disable Metrics/MethodLength
+      Rack::Builder.app do
+        use Rack::Cors do
+          allow do
+            origins 'http://localhost:3000'
+            resource '*',
+                     headers: :any,
+                     methods: %i[get post put delete options]
+          end
+        end
+
+        use OpenapiFirst::Middlewares::RequestValidation
+        use OpenapiFirst::Middlewares::ResponseValidation
+
+        map('/users') { run Web::Controllers::UsersController }
+      end
+    end
   end
 end
